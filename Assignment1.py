a9c779e63a740b971443d6e3c78251ae13583f35
ALOK SINGH <123860933+2004-AlokSINGH@users.noreply.github.com>
2024-07-04 17:56:56 +0530

    Update Assignment1.py

--- Assignment1.py
+++ Assignment1.py
@@ -1,1 +1,20 @@
-print("hello")
+# Create lower triangular, upper triangular and pyramid containing the "*" character.
+
+def pattern(n):
+  
+    print("Lower triangular pattern")
+  
+    for i in range(1,n+1):
+        print("*"*(i))
+        
+    print("Upper triangular pattern")
+  
+    for i in range(n,0,-1):
+        print(" "*(n-i)+"*"*(i))
+        
+    print("pyramid")
+  
+    for i in range(1,n+1,2):
+        print(" "*((n-i)//2)+"*"*i)
+
+pattern(6)
